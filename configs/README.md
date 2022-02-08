eb268615aa2fc1c42a87b0424ebefb8fc5171038
z3n <tyler@ethereum.org>
2022-02-08 13:39:07 -0600

    adjust configs and complete dockers build scripts and naming convention

--- configs/README.md
+++ configs/README.md
@@ -6,3 +6,5 @@ Example configs.
 2. Multi contain evenly distributed client examples.
  - phase0-altair: altair fork at epoch 4.
  - phase0-bellatrix: altair fork at epoch 4, bellatrix at 7.
+
+Configuration use image and tag information for deployment. For matching pairs you can use the build\_dockers.sh script in the dockers directory.
--- dockers/build_dockers.sh
+++ dockers/build_dockers.sh
@@ -1,18 +1,24 @@
 # Example local dockers for experiments
 
 # geth client
-docker build -t geth-client:master -f geth-client.Dockerfile
-# eth2-bootnode
-docker build -t bootnode-clients:latest -f bootnode-client.Dockerfile
+# docker build -t geth-client:master -f geth-client.Dockerfile
+# # eth2-bootnode
+# docker build -t bootnode-clients:latest -f bootnode-client.Dockerfile
+# 
+# #pre-bellatrix
+# docker build -t lighthouse-client:stable -f lighthouse-client_stable.Dockerfile
+# docker build -t nimbus-client:stable -f nimbus-client_stable.Dockerfile
+# docker build -t prysm-client:develop -f prysm-client_develop.Dockerfile
+# docker build -t teku-client:master -f teku-client_master.Dockerfile
+# 
+# # bellatrix dockers.
+# docker build -t lighthouse-client:kintsugi -f lighthouse-client_kintsugi.Dockerfile
+# docker build -t nimbus-client:kintsugi -f nimbus-client_kintsugi.Dockerfile
+# docker build -t prysm-client:kintsugi -f prysm-client_kintsugi.Dockerfile
+# docker build -t teku-client:kintsugi -f teku-client_kintsugi.Dockerfile
 
-#pre-bellatrix
-docker build -t lighthouse-client:stable -f lighthouse-client_stable.Dockerfile
-docker build -t nimbus-client:stable -f nimbus-client_stable.Dockerfile
-docker build -t prysm-client:develop -f prysm-client_develop.Dockerfile
-docker build -t teku-client:master -f teku-client_master.Dockerfile
-
-# bellatrix dockers.
-docker build -t lighthouse-client:kintsugi -f lighthouse-client_kintsugi.Dockerfile
-docker build -t nimbus-client:kintsugi -f nimbus-client_kintsugi.Dockerfile
-docker build -t prysm-client:kintsugi -f prysm-client_kintsugi.Dockerfile
-docker build -t teku-client:kintsugi -f teku-client_kintsugi.Dockerfile
+for df in $(ls | grep "Dockerfile"); do
+    x=`echo $df | tr "_" ":" `
+    image=${x::-11}
+    docker build -t $image -f $df .
+done
